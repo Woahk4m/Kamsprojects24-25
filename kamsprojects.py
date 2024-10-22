7449f32e357cd46fe240d27d46675d0f2748bf3b
Woahk4m <kamilarojas3542@hotmail.com>
2024-10-22 17:19:09 -0400

    update

---
+++ kamsprojects.py
@@ -0,0 +1,19 @@
+username = 'woahkam'
+def greet(username):
+    print("Welcome to the game, {username} !")
+
+# Get user input
+user_name = input("Please enter your name: ")
+greet_user(user_name)
+
+
+
+
+
+
+
+
+
+
+
+
