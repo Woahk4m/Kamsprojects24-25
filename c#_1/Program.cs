a1547e143bce3ff11bc4206fbe1fc1724e44e5b0
Woahk4m <kamilarojas3542@hotmail.com>
2025-01-16 18:04:58 -0500

    conditional statement added

--- c#_1/Program.cs
+++ c#_1/Program.cs
@@ -3,13 +3,23 @@ using System;
 
 class Program
 {
+    
     public static void Main()
     {
         Console.WriteLine("Hello, World!");
-        float myNum = 5.75F;
+        float myNum = 200.0F;
         Console.WriteLine(myNum);
         int myNum2 = 100000;
         Console.WriteLine(myNum);
+        if (myNum>100.00){
+            Console.WriteLine("the number is bigger than 100");
+
+        }
+        else{
+             Console.WriteLine("the number is small than 100");
+
+        }
+
     }
 }
 
--- c#_1/obj/Debug/net9.0/c#_1.AssemblyInfo.cs
+++ c#_1/obj/Debug/net9.0/c#_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70ac12925275558c2536d368af4418098817321c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48f9da1e4cc84dea9a81f8e743a263c0e32fe6e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
